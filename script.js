87716fb26838f64f2b21fbc6fe42491732782d39
itsbeenlowang <96687810+itsbeenlowang@users.noreply.github.com>
2023-07-06 08:52:52 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function copyBitcoinAddress() {
-  var btcAddress = document.getElementById('btc-address');
-  btcAddress.select();
-  document.execCommand('copy');
-  alert('Bitcoin address copied!');
-}
+        function copyToClipboard() {
+            const bitcoinAddress = document.getElementById('bitcoin-address');
+            const textarea = document.createElement('textarea');
+            textarea.value = bitcoinAddress.textContent;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand('copy');
+            document.body.removeChild(textarea);
+            alert('Bitcoin address copied to clipboard!');
+        }
