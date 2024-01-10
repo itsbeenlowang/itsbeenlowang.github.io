00514a1f6711be6f71e3f28ac127ca79c4b9c252
itsbeenlowang <96687810+itsbeenlowang@users.noreply.github.com>
2024-01-10 11:23:57 +0100

    10

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-function copyToClipboard(elementId) {
-    const addressElement = document.getElementById(elementId);
+document.addEventListener('DOMContentLoaded', function() {
+    const bitcoinCopyButton = document.querySelector('.bitcoin-copy-button');
+    const usdtCopyButton = document.querySelector('.usdt-copy-button');
 
-    if (addressElement) {
-        const textarea = document.createElement('textarea');
-        textarea.value = addressElement.textContent;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand('copy');
-        document.body.removeChild(textarea);
-        alert(`${elementId.toUpperCase()} address copied to clipboard!`);
+    if (bitcoinCopyButton && usdtCopyButton) {
+        bitcoinCopyButton.addEventListener('click', function() {
+            copyToClipboard('bitcoin-address');
+        });
+
+        usdtCopyButton.addEventListener('click', function() {
+            copyToClipboard('usdt-address');
+        });
     } else {
-        alert(`Invalid element ID: ${elementId}`);
+        console.error('Copy buttons not found.');
     }
-}
+
+    function copyToClipboard(addressId) {
+        const addressElement = document.getElementById(addressId);
+
+        if (addressElement) {
+            const textarea = document.createElement('textarea');
+            textarea.value = addressElement.textContent;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand('copy');
+            document.body.removeChild(textarea);
+            alert(`${addressId.toUpperCase()} address copied to clipboard!`);
+        } else {
+            alert(`Invalid address ID: ${addressId}`);
+        }
+    }
+});
