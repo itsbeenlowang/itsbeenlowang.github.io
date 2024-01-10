ee5d38c02121e80b9fd0c38ab206c75d7128ed9d
itsbeenlowang <96687810+itsbeenlowang@users.noreply.github.com>
2024-01-10 11:03:00 +0100

    13

--- script.js
+++ script.js
@@ -8,3 +8,14 @@
             document.body.removeChild(textarea);
             alert('Bitcoin address copied to clipboard!');
         }
+
+        function copyToClipboard() {
+            const usdtAddress = document.getElementById('usdt-address');
+            const textarea = document.createElement('textarea');
+            textarea.value = usdtAddress.textContent;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand('copy');
+            document.body.removeChild(textarea);
+            alert('USDT address copied to clipboard!');
+        }
